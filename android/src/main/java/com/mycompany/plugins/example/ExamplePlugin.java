d8b8bc04d41e6f377efedb90bd131ab4c3917920
Anže Matelič <amatelic93@gmail.com>
2021-08-25 13:10:38 +0200

    Fixed some configuration parts

--- android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
@@ -1,15 +1,27 @@
 package com.mycompany.plugins.example;
 
-import com.getcapacitor.JSObject;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.getcapacitor.JSArray;
+import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
+import com.zopim.android.sdk.api.ZopimChat;
+import com.zopim.android.sdk.model.VisitorInfo;
+import com.zopim.android.sdk.prechat.ZopimChatActivity;
+
+import org.json.JSONException;
 
 @CapacitorPlugin(name = "Example")
 public class ExamplePlugin extends Plugin {
 
-    private Example implementation = new Example();
+    @PluginMethod()
+    public void initialize(PluginCall call) {
+        String accountKey = String.valueOf(this.getConfigValue("accountKey"));
+        ZopimChat.init(accountKey);
+    }
 
     @PluginMethod
     public void echo(PluginCall call) {
@@ -18,5 +30,41 @@ public class ExamplePlugin extends Plugin {
         JSObject ret = new JSObject();
         ret.put("value", implementation.echo(value));
         call.resolve(ret);
+    }
+
+        @PluginMethod()
+    public void setVisitorInfo(PluginCall call) {
+        String name = call.getString("name");
+        String email = call.getString("email");
+        String phoneNumber = call.getString("phoneNumber");
+
+        VisitorInfo visitorData = new VisitorInfo.Builder()
+            .name(name)
+            .email(email)
+            .phoneNumber(phoneNumber)
+            .build();
+
+        // ZopimChat.setVisitorInfo(visitorData);
+    }
+
+    @PluginMethod()
+    public void open(PluginCall call) {
+        String department = call.getString("department");
+        JSArray tags = call.getArray("tags", new JSArray());
+
+        String[] tagsParameters = null;
+        try {
+            tagsParameters = tags.<String>toList().toArray(new String[0]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        AppCompatActivity activity = getActivity();
+
+        ZopimChat.SessionConfig config = new ZopimChat.SessionConfig()
+            .department(department)
+            .tags(tagsParameters);
+
+        ZopimChatActivity.startActivity(activity, config);
     }
 }
